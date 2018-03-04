86514fca3130dec4d0998d0b660f6675b79844d7
Colin Caine <cmc@cs.man.ac.uk>
2018-03-04 13:29:00 +0000

    Make regression less magic

--- brellenge_prep.py
+++ brellenge_prep.py
@@ -19,21 +19,24 @@ def change_since(start, end=2015):
     end = 'y{}'.format(end)
     return (pivoted.loc[:, end] - pivoted.loc[:, start]) / pivoted.loc[:, start]
 
-# Set up some globals
+# Read in data
 data = pd.read_csv("data/ethnicity-full-nested-new.csv")
 yearlabels = ['y{}'.format(x) for x in range(1998, 2018)]
 
-# Data for joining on: Remove all the ethnicity stuff
-metadata = data.drop(["ethnic2", "ethnicrate"] + yearlabels,1).drop_duplicates().set_index('AreaName')
-# Add a "Result" column for Tess.
-metadata["Result"] = metadata.apply(lambda row: "Leave" if row.Leave > row.Remain else "Remain", axis=1)
+## Globals to play with
 
 # Make the pivot table
 pivoted = pd.pivot_table(data[['AreaName', 'ethnic2'] + yearlabels], columns=['ethnic2'], index = ['AreaName'])
 
+# Data for joining on: Remove all the ethnicity stuff and drop rate_nonUK because it's got NAs in it.
+metadata = data.drop(["ethnic2", "ethnicrate", "rate_nonUk"] + yearlabels,1).drop_duplicates().set_index('AreaName')
+# Add a "Result" column for Tess.
+metadata["Result"] = metadata.apply(lambda row: "Leave" if row.Leave > row.Remain else "Remain", axis=1)
+# Proportion White British in 2015: good for regression with change rate.
+metadata['y2015_WBR'] = pivoted.loc[:, idx['y2015', 'White British']]
+
 # List of ethnicities for for loops.
 ethnicities = [x for x in pivoted.columns.levels[1]]
-
 
 ### Analysis ###
 
@@ -70,7 +73,11 @@ stacked_bar_chart() # I'm not sure why the bar chart does not remain constant
 
 # Regression example
 
-# import statsmodels.formula.api as smf
-# df=change_since(2005).join(metadata).dropna()
-# # The Q is to handle whitespace in column names.
-# smf.ols('Pct_Remain ~ Q("White British") + Q("White Other") + Asian + Black + Other + Region', data=df).fit().summary()
+import statsmodels.formula.api as smf
+df=change_since(2005).join(metadata)
+
+# R2 = 0.45
+# The Q is to handle whitespace in column names.
+smf.ols('Pct_Remain ~ Q("White British") + Q("White Other") + Asian + Black + Other + Region', data=df).fit().summary()
+# R2 = 0.45
+smf.ols('Pct_Remain ~ Q("White British") + Q("White Other") + Asian + Black + Other + y2015_WBR', data=df).fit().summary()
