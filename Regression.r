321898941138862eb04b5b86fed03b8de1d679b8
Heather A Robinson <robinsonheather@hotmail.co.uk>
2018-03-04 13:00:43 +0000

    Update Regression.r

--- Regression.r
+++ Regression.r
@@ -8,9 +8,9 @@ attach(test3)
 
 #WHEN WE DECIDE WHICH VARIABLES TO INCLUDE WE WILL UPDATE THE MODEL CODE
 RegModel.1 <- 
-  lm(Pct_Leave.x~rate_nonUK+age_grp18_24+age_grp25_34+age_grp35_44+age_grp45_54+age_grp55_64+
-  age_grp65_over+Area.Sq.Km+Country.of.Birth....Not.United.Kingdom..2011+change10+change05+
-  Economic.Activity..Unemployment.Rate..2011+Mid.2016.Population+Pct_Turnout.x+Rejected_Ballots
+  lm(Pct_Leave.x~rate_nonUk+age_grp18_24+age_grp25_34+age_grp35_44+age_grp45_54+age_grp55_64+
+  age_grp65_over+Area.Sq.Km+change10+change05+
+ Mid.2016.Population+Pct_Turnout.x+Rejected_Ballots+
   People.per.Sq.Km,  data=test3)
    
  #STEPWISE SIMPLIFICATION
