07d2a26669db50250f7060d2615abb25db2e974e
malteserteresa <tess_the_titan@hotmail.com>
2018-03-04 13:26:59 +0100

    added stacked bar plot

--- brellenge_prep.py
+++ brellenge_prep.py
@@ -11,18 +11,19 @@ Created on Sat Mar  3 11:14:51 2018
 """
 
 import pandas as pd
-import unittest
-import numpy as np
+
 
 def change_since(start, end=2015):
     "Normalised change in population since x till y."
-    start = f'y{start}'
-    end = f'y{end}'
+    start = 'y{}'.format(start)
+    end = 'y{}'.format(end)
     return (pivoted.loc[:, end] - pivoted.loc[:, start]) / pivoted.loc[:, start]
 
 # Set up some globals
-data = pd.read_csv("data/ethnicity-full-nested-new.csv")
-yearlabels = [f'y{x}' for x in range(1998, 2018)]
+data = pd.read_csv("/home/tcake/data_challenge/brexit_data_challenge/ethnicity-full-nested-new.csv")
+yearlabels = ['y{}'.format(x) for x in range(1998, 2018)]
+
+pivoted = pd.pivot_table(data[['AreaName', 'ethnic2'] + yearlabels], columns=['ethnic2'], index = ['AreaName'])
 
 # List of ethnicities for for loops.
 ethnicities = [x for x in pivoted.columns.levels[1]]
@@ -33,7 +34,6 @@ metadata = data.drop(["ethnic2", "ethnicrate"] + yearlabels,1).drop_duplicates()
 metadata["Result"] = metadata.apply(lambda row: "Leave" if row.Leave > row.Remain else "Remain", axis=1)
 
 # Make the pivot table
-pivoted = pd.pivot_table(data[['AreaName', 'ethnic2'] + yearlabels], columns=['ethnic2'], index = ['AreaName'])
 
 
 ### Analysis ###
@@ -49,12 +49,16 @@ pivoted.loc[:, idx['y1998':'y2003', 'Asian':'Other']]
 import matplotlib.pyplot as plt
 import seaborn
 
+def stacked_bar_chart():
+    pivoted.loc[:,idx[:,:]].dropna().sum().divide(348).unstack('ethnic2').plot.bar(stacked=True, figsize=(10,7), legend =True)
+
 def scatter_plot_change(ethnicity, start, end=2015):
     "Plot change in ethnicity since start against Pct_Remain"
     data = metadata.join(change_since(start, end)[ethnicity]).dropna()
     seaborn.regplot(data[ethnicity], data['Pct_Remain'])
 
 scatter_plot_change('White Other', 2003)
+stacked_bar_chart() # I'm not sure why the bar chart does not remain constant
 #plt.show()
 
 # Interactive use:
@@ -66,34 +70,4 @@ scatter_plot_change('White Other', 2003)
 # IPython.embed()
 
 
-### Old stuff ###
 
-# class FeatureEngineering:
-
-#      def percentage_NaN(dataframe):
-#         num = selected_columns.isnull().sum()
-#         den = len(selected_columns)
-#         percentage_by_column = round(num/den, 2)
-#         percentage_loss = percentage_by_column.sum()/len(percentage_by_column)
-#         return percentage_loss
-
-    # You can just use metadata.query('Remain < Leave'), if you want the result column I include a oneline below
-     # def label_result(dataframe):
-     #    dataframe["Result"] = pd.Series(np.NaN)
-     #    leave_index = dataframe[dataframe["Leave"] > dataframe["Remain"]].index
-     #    remain_index = dataframe[dataframe["Leave"] < dataframe["Remain"]].index
-     #    for i in leave_index:
-     #        dataframe.loc[i,"Result"] = "Leave"
-     #    for i in remain_index:
-     #        dataframe.loc[i,"Result"] = "Remain"
-     #    return dataframe
-
-# Run the tests with `pytest brellenge_prep.py`
-# class Test(unittest.TestCase):
-
-#     def test_result_labelled_correctly(self):
-#         data.index = data.AreaName
-#         self.assertEqual("Leave", data.loc['Hartlepool','Result'].values[0])
-
-#     def test_no_data_lost(self):
-#         self.assertEqual(FeatureEngineering.percentage_NaN(data), FeatureEngineering.percentage_NaN(selected_columns))
